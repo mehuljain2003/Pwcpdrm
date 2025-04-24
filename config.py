f2bffea03073aa48c518242accc1a09bf0a6c937
mehuljain2003 <85852792+mehuljain2003@users.noreply.github.com>
2025-04-24 23:28:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "api id"
 api_hash = "82fb5cd2de25e4c01021b8bbae9909bb"
 bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+auth_users = [6697397532]
+sudo_users = [7721185290]
